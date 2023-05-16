b87be8a539d6ed3c8c5b2c37a0f5d86e3700eb1a
MuthonduG <muthondugithinji@gmail.com>
2023-05-16 22:46:59 +0300

    user migration

---
+++ db/migrate/20230516194528_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :username
+      t.string :email
+      t.string :description
+      t.binary :avatar
+      t.boolean :admin
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
