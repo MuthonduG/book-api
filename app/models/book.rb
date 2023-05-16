352020bc116236607b879e2eca6296058dae19ce
MuthonduG <132641997+MuthonduG@users.noreply.github.com>
2023-05-17 00:23:32 +0300

    Update book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+    validates: title, presence: true
+    validates: description, presence: true
+    validates: poster, presence: true
+    validates: description, presence: true
+    validates: price, presence: true, numericality: true
+
+    has_many :favourites
+    has_many :users, through: :favourites
 end
